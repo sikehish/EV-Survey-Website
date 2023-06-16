4afbd5adcdac1447511945930c0efd6d110f7f6e
Syed Hisham Akmal <hisham0502@gmail.com>
2023-06-16 14:42:57 +0530

    Added pie chart

--- python/analysis.py
+++ python/analysis.py
@@ -6,11 +6,16 @@ plt.rcParams["figure.autolayout"] = True
 
 df = pd.read_csv("env.csv")
 
-model_counts = df["Model"].value_counts()
+# model_counts = df["Model"].value_counts()
+type_counts = df["Vehicle"].value_counts()
 
 # Plotting a pie chart
-plt.pie(model_counts, labels=model_counts.index, autopct="%1.1f%%")
-plt.title("Vehicle Models")
+# plt.pie(model_counts, labels=model_counts.index, autopct="%1.1f%%")
+# plt.title("Vehicle Models")
+# plt.axis("equal")
+
+plt.pie(type_counts, labels=type_counts.index, autopct="%1.1f%%")
+plt.title("Vehicle Type")
 plt.axis("equal")
 
 # Display the chart
